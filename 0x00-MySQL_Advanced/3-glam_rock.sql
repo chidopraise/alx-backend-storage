f743eef8a9d9af8db02efe75cdff9ea77ec176af
Abraham Livinus <abrahamlivinus@gmail.com>
2024-08-08 02:12:20 -0700

    3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,13 +1,4 @@
--- Create the query to select and compute the lifespan of Glam rock bands
-SELECT 
-    name AS band_name, 
-    CASE 
-        WHEN split IS NOT NULL THEN YEAR('2022-12-31') - formed
-        ELSE YEAR('2022-12-31') - formed
-    END AS lifespan
-FROM 
-    metal_bands
-WHERE 
-    style = 'Glam rock'
-ORDER BY 
-    lifespan DESC;
+--  SQL script that lists all bands with Glam rock as their main style,
+-- ranked by their longevity
+
+SELECT band_name, (IFNULL(split, 2020) - formed) AS lifespan FROM metal_bands WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
